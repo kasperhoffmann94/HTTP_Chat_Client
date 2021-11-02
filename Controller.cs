ac15764956b00014148116cd3ea020b6e4d2263d
Kasper Malmsiø Hoffmann <43535701+kasperhoffmann94@users.noreply.github.com>
2021-11-02 15:15:29 +0100

    Create Chat client with socket and TCP/IP communication

---
+++ Controller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace HTTP_Chat_Client
+{
+    public class Controller
+    {
+        public EventHandler _responseEventHandler;
+        private readonly Socket _socket;
+
+        public Controller(string adress, int port)
+        {
+            IPAddress ipAddress = IPAddress.Parse(adress);
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+
+            _socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _socket.Connect(localEndPoint);
+
+            Thread listeningThread = new Thread(ListenToServer);
+            listeningThread.Start();
+        }
+
+        public void SendMessage(string message)
+        {
+            byte[] messageSent = Encoding.ASCII.GetBytes(message + "<EOF>");
+            int byteSent = _socket.Send(messageSent);
+        }
+
+        public void ListenToServer()
+        {
+            bool running = true;
+            while (running)
+            {
+                byte[] messageReceived = new byte[1024];
+                int byteReceived = _socket.Receive(messageReceived);
+                string message = Encoding.ASCII.GetString(messageReceived, 0, byteReceived);
+                message = message.Substring(0, message.Length - 5);
+                _responseEventHandler?.Invoke(message, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HTTP_Chat_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Controller _controller;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateMessageReceivedTextBox(object sender, EventArgs e)
+        {
+            string message = sender.ToString();
+            Dispatcher?.InvokeAsync(() => MessageReceived.AppendText(message));
+            Dispatcher?.InvokeAsync(() => MessageReceived.AppendText(Environment.NewLine));
+            Dispatcher?.InvokeAsync(MessageReceived.ScrollToEnd);
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            string message = SendMessage.Text;
+
+            _controller?.SendMessage(message);
+        }
+
+        private void ConnectButtonClick(object sender, RoutedEventArgs e)
+        {
+            string address = AddressBox.Text;
+            int port = int.Parse(PortBox.Text);
+            _controller = new Controller(address, port);
+
+            _controller._responseEventHandler += UpdateMessageReceivedTextBox;
+
+            _controller.SendMessage(UserNameBox.Text);
+
+            SendButton.IsEnabled = true;
+            ConnectButton.IsEnabled = false;
+            UserNameBox.IsEnabled = false;
+        }
+    }
+}
